fb2e5066836d3ba849e13f43553c494ab2ee62ce
Igor Alegria <igor.alegria@gmail.com>
2023-03-17 17:10:42 -0300

    Finalizando lint

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -5,18 +5,19 @@ class Feedback extends React.Component {
   render() {
     const { assertions } = this.props;
     const totalAssertions = 3;
-      return (
-        <>
-          <h1>feedback</h1>
-          { assertions < totalAssertions ? (<p data-testid='feedback-text'>Could be better...</p>) 
-          : (<p data-testid='feedback-text'>Well Done</p>)}
-        </>
+    return (
+      <>
+        <h1>feedback</h1>
+        { assertions < totalAssertions
+          ? (<p data-testid="feedback-text">Could be better...</p>)
+          : (<p data-testid="feedback-text">Well Done</p>)}
+      </>
     );
   }
 }
 
 const mapStateToProps = (user) => ({
-    assertions: user.assertions,
-  });
+  assertions: user.assertions,
+});
 
 export default connect(mapStateToProps)(Feedback);
