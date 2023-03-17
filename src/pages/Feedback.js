3cce210f230bcf88a070e112f9a7419512fa7079
Igor Alegria <igor.alegria@gmail.com>
2023-03-17 16:59:47 -0300

    Arrumando lint

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class Feedback extends React.Component {
-    render() {
-       const { assertions } = this.props;
- 
-        return (
+  render() {
+    const { assertions } = this.props;
+      return (
             <>
             <h1>feedback</h1>
             { assertions < 3 ? (<p data-testid='feedback-text'>Could be better...</p>) : (<p data-testid='feedback-text'>Well Done</p>)}
