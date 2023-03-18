24c3ae172ee6982a33c04cfba3f23b7538b670cb
Mateus <mateusppa10@gmail.com>
2023-03-17 21:21:31 -0300

    problema

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,13 +1,101 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import { API_GAME } from '../services/APIPlayer';
 
 export default class Game extends Component {
+  state = {
+    questions: [],
+  };
+
+  async componentDidMount() {
+    const { history } = this.props;
+    const errorCode = 3;
+    const token = localStorage.getItem('token');
+    const data = await API_GAME(token);
+    if (data.response_code === errorCode) {
+      localStorage.clear();
+      history.push('/');
+    }
+    const questions = data.results.map((question, i) => {
+      if (i === 0) {
+        question.show = true;
+      } else {
+        question.show = false;
+      }
+      // const allQuestions = [question.correct_answer, ...question.incorrect_answers];
+      // console.log('🚀 ~ file: Game.js:27 ~ Game ~ questions ~ allQuestions:', allQuestions)
+      // question.sortQuestions = allQuestions.sort();
+      const options = [question.correct_answer, question.incorrect_answers];
+      question.sortOptions = options.sort();
+      return question;
+    });
+    this.setState({ questions });
+  }
+
   render() {
+    const { questions } = this.state;
     return (
       <>
         <Header />
-        <div>Game</div>
+        <main>
+          {questions.map((question, i) => question.show && (
+            <div key={ i }>
+              <div data-testid="question-category">{question.category}</div>
+              <div data-testid="question-text">{question.question}</div>
+              {/* <button data-testid="correct-answer">
+                {question.correct_answer}
+              </button>
+              {question.incorrect_answers.map((incorrectQuestion, index) => (
+                <button
+                  data-testid={ `wrong-answer-${i}` }
+                  key={ index }
+                >
+                  {incorrectQuestion}
+                </button>
+              ))} */}
+              {/* <div data-testid="answer-options">
+                {question.sortQuestions.map((sortQuestion, index) => (
+                  <button
+                    key={ index }
+                    data-testid={
+                      `${sortQuestion === question.correct_answer
+                        ? 'correct-answer' : `wrong-answer-${index}`}`
+                    }
+                  >
+                    {sortQuestion}
+
+                  </button>
+                ))}
+              </div> */}
+              <div data-testid="answer-options">
+                {question.sortOptions.map((option, allIndex) => (
+                  option === question.correct_answer
+                    ? <button data-testid="correct-answer" key={ allIndex }>
+                      {option}
+                    </button>
+                    : option.map((incorrect, incorrectIndex) => (
+                      <button
+                        data-testid={ `wrong-answer-${incorrectIndex}` }
+                        key={ incorrectIndex }
+                      >
+                        {' '}
+                        {incorrect}
+                      </button>
+                    ))
+                ))}
+              </div>
+            </div>
+          ))}
+        </main>
       </>
     );
   }
 }
+
+Game.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+  dispatch: PropTypes.func,
+}.isRequired;
--- src/services/APIPlayer.js
+++ src/services/APIPlayer.js
@@ -1,7 +1,14 @@
-const URL_API = 'https://opentdb.com/api_token.php?command=request';
+const URL_TOKEN = 'https://opentdb.com/api_token.php?command=request';
 
 export const API_PLAYER = async () => {
-  const response = await fetch(URL_API);
+  const response = await fetch(URL_TOKEN);
+  const data = await response.json();
+  return data;
+};
+
+export const API_GAME = async (token) => {
+  const urlGame = `https://opentdb.com/api.php?amount=5&token=${token}`;
+  const response = await fetch(urlGame);
   const data = await response.json();
   return data;
 };
